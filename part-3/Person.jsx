3e595ef296274ff26b5600ca064c959a0405788b
jessicaelee <jessica.e.lee.93@gmail.com>
2020-02-24 11:56:44 -0800

    bootstrap

--- part-3/Person.jsx
+++ part-3/Person.jsx
@@ -11,3 +11,20 @@ function Person(props) {
 
     </div>)
 }
+
+function Alert(props) {
+    const { message, type } = props;
+    let style;
+    if (type === "success") {
+        style = { backgroundColor: "#D4EDDA", color: "#155724", border: "1px solid #C3E6CB" }
+    } else if (type === "danger") {
+        style = { backgroundColor: "#F8D7DA", color: "#721C24", border: "1px solid #F5C6CB" }
+    }
+    style.width = "100%";
+    style.borderRadius = "5px";
+    style.padding = "12px 20px";
+    style.fontFamily = "Arial";
+
+    return <div style={style}> {message}</div>
+
+}
--- part-3/app.jsx
+++ part-3/app.jsx
@@ -1,4 +1,8 @@
 ReactDOM.render(
-    <Person name="will" age={32} hobbies={["skiing", "drinking"]} />,
+    <div>
+        <Person name="will" age={32} hobbies={["skiing", "drinking"]} />
+        <Alert type="success" message="Good Job!" />
+        <Alert type="danger" message="You're in danger!" />
+    </div>,
     document.getElementById("root")
 )
